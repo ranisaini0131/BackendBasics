e2e9c6b7afe774caf645ed01d8c66fb96cdab6a3
Rani Saini <sainirani1797@gmail.com>
2023-11-29 21:11:36 +0530

    add user and video model

--- Youtube_Backend/readme.md
+++ Youtube_Backend/readme.md
@@ -11,3 +11,12 @@
 => middlewares app.use() k through use hoti h
 =>body-parser express me included h
 =>cors allow us to seeting across cross origin resource sharing
+
+=>npm i mongoose-aggregate-paginate-v2 aggregation piplines of mongoose used in production
+
+=> bcrypt and bcryptjs both are same, bcrypt is just optimized with 0 dependencies and compatible with bcrypt,
+It hashed our pasword
+
+=> pre is a type of hook in mongoose which provides the functionality of performing some task just before the any defined opt, we can't use arrow func in pre, becuase "this" doesn't the context(userSchema) in arrow func, so we use regular func
+
+=> JWT bearer token means whoever bears it we gives the access to them
---
+++ Youtube_Backend/src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowerCase: true,
+        trim: true,
+        index: true //for anabling searching 
+    },
+    email: {
+        type: String,
+        required: true,
+        lowerCase: true,
+        unique: true,
+        trim: true,
+    },
+
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+
+    },
+
+    avatar: {
+        type: String, //for stroing url, cloudinary url
+        required: true
+    },
+
+    coverImage: {
+        type: String
+    },
+
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+
+    refreshTokens: {
+        type: String
+    }
+
+},
+    {
+        timestamps: true
+    })
+
+userSchema.pre("save", async function (next) {
+    //checking if password is modified or not, if modify then bcrypt it and not then next()
+    if (!this.isModified("password")) return next();
+
+    //encrypting password
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
+
+
+//checking password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            //payload
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            //payload
+            _id: this._id,
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
